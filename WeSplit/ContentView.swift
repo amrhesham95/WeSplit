c1b9cf9604c33ec3ec419cdfbb5a3d4a31f3a6ba
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-10-29 21:25:24 +0200

    Creating Picker and text field

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let tipPercentages = [10, 15, 20, 25, 0]
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @State var formatter: Formatter = {
+        let formatter = NumberFormatter()
+        formatter.currencyCode = "USD"
+        return formatter
+    }()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number Of People: ", selection: $numberOfPeople) {
+                        ForEach(2..<100, id: \.self) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
