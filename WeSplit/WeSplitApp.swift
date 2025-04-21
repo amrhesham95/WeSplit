f3092c17e7f4a15745163b3317520587faabe718
AmrHassan95 <amr.hassan.2@deliveryhero.com>
2025-04-21 15:01:59 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Amr Hassan on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
